9f0bc55ae23fa27c0e929cd67fd05460f516c353
Ibnballo1 <belloabdullateef035@gmail.com>
2024-05-23 02:37:37 +0100

    Expandible component complete

--- src/components/Expandible.tsx
+++ src/components/Expandible.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 interface Props {
   children: string;
   maxChar?: number;
 }
 
 const Expandible = ({ children, maxChar = 100 }: Props) => {
-  return <div>{children}</div>;
+  const [isExpand, setIsExpand] = useState(false);
+  const text = isExpand ? children : children.substring(0, maxChar);
+  return (
+    <div>
+      {text}...{" "}
+      <button onClick={() => setIsExpand(!isExpand)}>
+        {isExpand ? "less" : "more"}
+      </button>
+    </div>
+  );
 };
 
 export default Expandible;
